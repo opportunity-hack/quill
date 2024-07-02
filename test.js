62c2d5362918c5fbc03216aaeef4451e8b51bb23
ASHMITHA PAIS <ashdale30@gmail.com>
2024-07-01 20:03:19 -0700

    Made changes to use resend SMTP with nodemailer.

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
 const request = require("supertest");
 const app = require("./app.js");
 
-
 describe("Get Login", () => {
-    it("Should Get the login Page   ", async () => {
-        const res = await request(app)
-            .get("/login");
-        expect(res.statusCode).toEqual(200);
-    });
+  it("Should Get the login Page", async () => {
+    const res = await request(app).get("/login");
+    expect(res.statusCode).toEqual(200);
+  });
 });
